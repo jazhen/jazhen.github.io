7f35569a9815a3d7e5e08b07e7738d9507235a77
Jason Zhen <jasonzhen.mail@gmail.com>
2021-08-22 03:18:08 -0700

    Choose avatar responsive sizing

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,11 +12,6 @@ const HeroContainer = styled.div`
   align-items: center;
   justify-content: center;
   padding: clamp(1em, 0.4em + 3vw, 4em);
-  gap: clamp(1em, 0.4em + 3vw, 4em);
-
-  @media (min-width: 50rem) {
-    flex-direction: row;
-  }
 `;
 
 const Avatar = styled(Image)`
@@ -24,7 +19,7 @@ const Avatar = styled(Image)`
   max-width: 60%;
 
   @media (min-width: 50rem) {
-    max-width: min(40%, 25rem);
+    max-width: min(40%, 25em);
   }
 `;
 
@@ -38,17 +33,25 @@ const HeroContent = styled.div`
 
 const HeroH1 = styled(H1)`
   font-size: clamp(2.827rem, 1.7937999999999998rem + 5.166vw, 7.993rem);
+  line-height: 1.167;
   color: #6200ee;
 `;
 
 const HeroH2 = styled(H2)`
   font-size: clamp(1.999rem, 1.5992000000000002rem + 1.999vw, 3.998rem);
+  line-height: 1.2;
   opacity: 0.87;
+  margin-bottom: 0.25em;
+
+  @media (min-width: 50rem) {
+    margin-bottom: 0.125em;
+  }
 `;
 
 const HeroH3 = styled(H3)`
   font-size: clamp(1rem, 0.8002rem + 0.9990000000000001vw, 1.999rem);
   opacity: 0.87;
+  margin-bottom: 1em;
 `;
 
 const HeroActions = styled.div``;
@@ -62,6 +65,8 @@ const Button = styled.button`
   border: 2px solid #6200ee;
   color: #6200ee;
   font-size: clamp(1rem, 0.95rem + 0.25vw, 1.25rem);
+  letter-spacing: 0.02857em;
+  font-weight: 500;
 
   &:hover {
     cursor: pointer;
@@ -73,22 +78,35 @@ const Button = styled.button`
   }
 `;
 
+const HeroContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: clamp(1em, 0.4em + 3vw, 4em);
+
+  @media (min-width: 50rem) {
+    flex-direction: row;
+  }
+`;
+
 const HeroSection = () => (
   <HeroContainer>
-    <Avatar src="https://jazhen.github.io/images/Jason_Zhen.png" />
+    <HeroContentContainer>
+      <Avatar src="https://jazhen.github.io/images/Jason_Zhen.png" />
 
-    <HeroContent>
-      <HeroH1>Jason Zhen</HeroH1>
-      <HeroH2>Full Stack Software Engineer</HeroH2>
-      <HeroH3>
-        Recent graduate of @UC&nbsp;Davis, @App&nbsp;Academy. Currently
-        available for opportunities.
-      </HeroH3>
-      <HeroActions>
-        <Button>contact</Button>
-        <Button>résumé</Button>
-      </HeroActions>
-    </HeroContent>
+      <HeroContent>
+        <HeroH1>Jason Zhen</HeroH1>
+        <HeroH2>Full Stack Software Engineer</HeroH2>
+        <HeroH3>
+          Recent graduate of @UC&nbsp;Davis, @App&nbsp;Academy. Currently
+          available for opportunities.
+        </HeroH3>
+        <HeroActions>
+          <Button>contact</Button>
+          <Button>résumé</Button>
+        </HeroActions>
+      </HeroContent>
+    </HeroContentContainer>
   </HeroContainer>
 );
 
