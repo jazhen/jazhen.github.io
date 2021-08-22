7de432d5e349fbf53fa29fc4c9839f19fea41904
Jason Zhen <jasonzhen.mail@gmail.com>
2021-08-22 14:29:41 -0700

    Change icons default size

--- src/components/Icons/ExternalLinkIcon.js
+++ src/components/Icons/ExternalLinkIcon.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-const ExternalLinkIcon = ({ size }) => (
+const ExternalLinkIcon = ({ strokeWidth, width }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth={strokeWidth}
     viewBox="0 0 24 24"
-    width={size === "medium" ? 32 : 48}
-    height={size === "medium" ? 32 : 48}
+    width={width}
+    height="auto"
   >
     <path stroke="none" d="M0 0h24v24H0z" />
     <path d="M11 7H6a2 2 0 00-2 2v9a2 2 0 002 2h9a2 2 0 002-2v-5M10 14L20 4M15 4h5v5" />
@@ -17,11 +17,13 @@ const ExternalLinkIcon = ({ size }) => (
 );
 
 ExternalLinkIcon.propTypes = {
-  size: PropTypes.string,
+  strokeWidth: PropTypes.string,
+  width: PropTypes.string,
 };
 
 ExternalLinkIcon.defaultProps = {
-  size: "medium",
+  strokeWidth: "1.5",
+  width: "30",
 };
 
 export default ExternalLinkIcon;
